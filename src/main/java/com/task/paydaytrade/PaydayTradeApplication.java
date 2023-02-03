be52cdebcd0ca66bc20503767192a11b365c6b28
Shafa00 <sefammmdv00@gmail.com>
2023-02-03 17:19:24 +0400

    Initial commit

--- src/main/java/com/task/paydaytrade/PaydayTradeApplication.java
+++ src/main/java/com/task/paydaytrade/PaydayTradeApplication.java
@@ -1,9 +1,13 @@
 package com.task.paydaytrade;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT3M")
 @EnableFeignClients
 @SpringBootApplication
 public class PaydayTradeApplication {
